2064db6cb9637ec7f37123ed6944f6c75e0a95c9
Hardforehead <kochegar005@gmail.com>
2023-02-01 21:53:02 +0300

    feat: dohuya chego added

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-const attack = document.querySelector('.attack_button');
+const attackButton = document.querySelector('.attack_button');
+const reloadButton = document.querySelector('.reload_button');
 const bullet = document.querySelector('.bullet');
-//let snd1 = new Audio("../assets/6f578c03d698a4c.mp3");
+const antagonistQuote = document.querySelector('.antagonist_quote');
+const protagonistQuote = document.querySelector('.protagonist_quote');
 
-attack.addEventListener('click', () => {
+let snd1 = new Audio("../assets/shoot.mp3");
+let snd2 = new Audio("../assets/reload.mp3");
+let bulletCount = 1;
+function shoot() {
     bullet.style = 'transform: translate(0, -800%)';
     bullet.classList.add('hidden');
+    antagonistQuote.classList.remove('hidden');
+    protagonistQuote.classList.remove('hidden');
+    snd1.currentTime = 0;
+    snd1.play();
+}
+function reload() {
+    bullet.style = 'transform: translate(0, 0)';
+    bullet.classList.remove('hidden');
+    antagonistQuote.classList.add('hidden');
+    protagonistQuote.classList.add('hidden');
+    snd2.currentTime = 0;
+    snd2.play();
+    bulletCount = 1;
+}
+
+attackButton.addEventListener('click', () => {
+    if (bulletCount > 0) {
+        shoot();
+        bulletCount--;
+    }
+})
+reloadButton.addEventListener('click', () => {
+    if (bulletCount < 1) {
+        reload();
+    }
 })
