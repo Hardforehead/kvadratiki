2c9bb0ee95c6c20f89d2cc72b70a89ebe8cb65a8
Hardforehead <kochegar005@gmail.com>
2023-02-02 19:19:37 +0300

    feat: recoil animation added

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const reloadButton = document.querySelector('.reload_button');
 const bullet = document.querySelector('.bullet');
 const antagonistQuote = document.querySelector('.antagonist_quote');
 const protagonistQuote = document.querySelector('.protagonist_quote');
+const protagonist = document.querySelector('.protagonist');
+const antagonist = document.querySelector('.antagonist');
 
 let snd1 = new Audio("assets/shoot.mp3");
 let snd2 = new Audio("assets/reload.mp3");
 let bulletCount = 1;
 function shoot() {
-    bullet.style = 'transform: translate(0, -800%)';
+    let a = Math.round(Math.random() * 100);
+    bullet.style = `transform: translate(${a % 2 === 0 ? a : -a}%, -900%)`;
+    protagonist.style = 'animation: x 0.3s cubic-bezier(0,20,1,20) 1';
     bullet.classList.add('hidden');
     antagonistQuote.classList.remove('hidden');
     protagonistQuote.classList.remove('hidden');
@@ -18,6 +22,7 @@ function shoot() {
 function reload() {
     bullet.style = 'transform: translate(0, 0)';
     bullet.classList.remove('hidden');
+    protagonist.style = 'animation: 0';
     antagonistQuote.classList.add('hidden');
     protagonistQuote.classList.add('hidden');
     snd2.currentTime = 0;
