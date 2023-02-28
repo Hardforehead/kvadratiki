c47031080b0ff4816ecbf2dac0d5ae7b48e1d202
Hardforehead <kochegar005@gmail.com>
2023-02-28 18:38:12 +0300

    feat: bullet upgrades added

--- script.js
+++ script.js
@@ -16,12 +16,15 @@ const popup = document.querySelector('.popup');
 const store = document.querySelector('.store_button');
 const item = document.querySelector('.item');
 const money = document.querySelector('.money');
-let snd1 = new Audio("assets/shoot.mp3");
-let snd2 = new Audio("assets/reload.mp3");
-let snd3 = new Audio("assets/winsound.mp3");
-let snd4 = new Audio("assets/restart.mp3");
-let snd5 = new Audio("assets/rikoshet.mp3");
-let score = 5;
+const bullet2 = document.querySelector('.bullet2');
+let shootSnd = new Audio("assets/shoot.mp3");
+let reloadSnd = new Audio("assets/reload.mp3");
+let winSnd = new Audio("assets/winsound.mp3");
+let restartSnd = new Audio("assets/restart.mp3");
+let rikoshetSnd = new Audio("assets/rikoshet.mp3");
+let buySnd = new Audio("assets/buy.mp3");
+let failSnd = new Audio("assets/fail.mp3");
+let score = 200;
 let bulletCount = 1;
 let maxhp = 5;
 let curhp = maxhp;
@@ -29,6 +32,8 @@ let prmaxhp = 5;
 let prcurhp = prmaxhp;
 let bbye = 1;
 let prdmg = 1;
+let bulletBorder = 4;
+money.innerHTML = `${score} деняк`;
 hpCount.innerHTML = `${curhp} / ${maxhp}`;
 prhpCount.innerHTML = `${prcurhp} / ${prmaxhp}`;
 function shoot() {
@@ -36,33 +41,33 @@ function shoot() {
     let b = Math.round(Math.random() * 100);
     let c = Math.round(Math.random() * 3);
     let d = Math.round(Math.random() * 3);
-    snd5.volume = 0.5;
-    bullet[1].style = `transform: translate(${a % 2 === 0 ? a : -a}%, -900%)`;
-    bullet[0].style = `transform: translate(${b % 2 === 0 ? b : -b}%, 900%)`;
+    rikoshetSnd.volume = 0.5;
+    bullet[1].style.transform = `translate(${a % 2 === 0 ? a : -a}%, -900%)`;
+    bullet[0].style.transform = `translate(${b % 2 === 0 ? b : -b}%, 900%)`;
     bullet[1].classList.add('hidden');
     bullet[0].classList.add('hidden');
 
     protagonist.style = `animation: x 0.3s cubic-bezier(0,20,1,20) ${d === 1 ? 1 : 2}`;
     antagonist.style = `animation: y 0.3s cubic-bezier(0,20,1,20) ${c === 1 ? 1 : 2}`;
-    
+
     if (c !== 1) {
         curhp < prdmg ? curhp = 0 : curhp -= prdmg;
         hp.style = `width: ${curhp / maxhp * 100}%`;
         hpCount.innerHTML = `${curhp} / ${maxhp}`;
-        
+
     } else {
-        snd5.currentTime = 0;
-        snd5.play();
+        rikoshetSnd.currentTime = 0;
+        rikoshetSnd.play();
     }
     if (d !== 1) {
         prcurhp--;
         prhp.style = `width: ${prcurhp / prmaxhp * 100}%`;
         prhpCount.innerHTML = `${prcurhp} / ${prmaxhp}`;
     } else {
-        snd5.currentTime = 0;
-        snd5.play();
+        rikoshetSnd.currentTime = 0;
+        rikoshetSnd.play();
     }
-    
+
     if (curhp === 0 || prcurhp === 0) {
         restartButton.classList.remove('hidden');
         if (curhp === 0) {
@@ -70,35 +75,37 @@ function shoot() {
             win.style.color = 'green';
             score += 5;
             money.innerHTML = `${score} деняк`;
-            snd3.currentTime = 0;
-            snd3.play();
+            winSnd.currentTime = 0;
+            winSnd.play();
         }
         if (prcurhp === 0) {
             win.innerHTML = 'поражение';
             win.style.color = 'red';
+            score += 1;
+            money.innerHTML = `${score} деняк`;
         }
         if (prcurhp === 0 && curhp === 0) {
             win.innerHTML = 'ничья';
             win.style.color = 'orange';
-            score += 2;
+            score += 3;
             money.innerHTML = `${score} деняк`;
         }
         win.classList.remove('hidden');
     }
-    snd1.currentTime = 0;
-    snd1.play();
+    shootSnd.currentTime = 0;
+    shootSnd.play();
 }
 function reload() {
-    bullet[1].style = 'transform: translate(0, 0)';
+    bullet[1].style.transform = 'translate(0, 0)';
     bullet[1].classList.remove('hidden');
-    bullet[0].style = 'transform: translate(0, 0)';
+    bullet[0].style.transform = 'translate(0, 0)';
     bullet[0].classList.remove('hidden');
     protagonist.style = 'animation: 0';
     antagonist.style = 'animation: 0';
     //antagonistQuote.classList.add('hidden2');
     //protagonistQuote.classList.add('hidden2');
-    snd2.currentTime = 0;
-    snd2.play();
+    reloadSnd.currentTime = 0;
+    reloadSnd.play();
     bulletCount = 1;
     if (curhp === 0) {
         restartButton.classList.remove('hidden');
@@ -128,29 +135,39 @@ restartButton.addEventListener('click', () => {
     prhpCount.innerHTML = `${prcurhp} / ${prmaxhp}`;
     restartButton.classList.add('hidden');
     win.classList.add('hidden');
-    snd4.currentTime = 0;
-    snd4.play();
+    restartSnd.currentTime = 0;
+    restartSnd.play();
 });
 
 store.addEventListener('click', () => {
-    popup.style = 'top: 100px'; 
-    shad.style.cssText =`background-color: rgba(20, 20, 20, 0.4);
+    popup.style = 'top: 100px';
+    shad.style.cssText = `background-color: rgba(20, 20, 20, 0.4);
     visibility: visible;
     z-index: 1;`;
 });
 shad.addEventListener('click', () => {
-    popup.style = 'top: -700px;'; 
-    shad.style.cssText =`background-color: rgba(20, 20, 20, 0);
+    popup.style = 'top: -700px;';
+    shad.style.cssText = `background-color: rgba(20, 20, 20, 0);
             visibility: hidden;
             z-index: 1;`;
 
     a = 0;
 });
 item.addEventListener('click', () => {
-    prdmg = 2;
-    item.style.background = 'lightgreen';
-    bullet[1].classList.add('bullet2');
-    score -= 5;
-    money.innerHTML = `${score} деняк`;
+    if (score > 0) {
+        prdmg += 1;
+        buySnd.currentTime = 0;
+        buySnd.play();
+        bulletBorder += 1;
+        if (bulletBorder < 16) {
+            bullet2.style = `border: ${bulletBorder}px solid rgb(162, 122, 0);`;
+            bullet[1].style = `border: ${bulletBorder}px solid rgb(162, 122, 0);`;
+        }
+        score -= 5;
+        money.innerHTML = `${score} деняк`;
+    } else {
+        failSnd.currentTime = 0;
+        failSnd.play();
+    }
 
 })
